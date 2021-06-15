ce2148f41836abea24d85e7f5f8515329a221f75
Nick Weber <webernick@icloud.com>
2021-06-15 10:15:15 -0700

    works

---
+++ add_LiDAR.py
@@ -0,0 +1,17 @@
+# ----- File picker and path to directory of LiDAR ------
+message_text = 'Select a directory of '
+iface.messageBar().pushMessage(message_text, duration=5)
+work_path = QFileDialog.getExistingDirectory()
+
+# ---- get a list of the directories ----
+dir_list = os.listdir(work_path)
+print(len(dir_list))
+
+
+# ---- loop to add all LiDAR tiles ----
+for dir in dir_list:
+    lidar_path = os.path.join(work_path, dir, dir + '.tif')
+    print(lidar_path)
+    iface.addRasterLayer(lidar_path)
+
+
